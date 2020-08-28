be1320cf92a5bac6a45b79ec6f27bd3ee8af3d3d
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-08-28 17:57:26 +0200

    Add images AND snackbar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import './styles.scss';
 import { MDCSwitch } from '@material/switch';
+import { MDCSnackbar } from '@material/snackbar';
+
+const config = {
+  chances: {
+    mousemove: {
+      total: 100,
+      active: 20,
+    },
+  },
+  distanceMinToTriggerAction: 50,
+  initialBasicMoves: 3,
+};
 
 let actionCounter = 0;
 let isMouseFarEnough = false;
@@ -20,8 +32,20 @@ function getRandomProbabilities(multiplier: number = 100): number {
 function randomMousemoveAction() {
   setTimeout(() => {
     setSwitchOff();
-  }, 2000);
+  }, 1000);
   console.log('randomMousemoveAction');
+}
+
+function manageFirstMousemove(): void {
+  if (!window.localStorage.getItem('isFirstMousemove')) {
+    const snackbarEl = document.querySelector('.mdc-snackbar');
+    if (!snackbarEl) return;
+
+    const snackbar = new MDCSnackbar(snackbarEl);
+    snackbar.open();
+
+    window.localStorage.setItem('isFirstMousemove', 'false');
+  }
 }
 
 function manageMousemoveAction(evt: MouseEvent) {
@@ -31,9 +55,8 @@ function manageMousemoveAction(evt: MouseEvent) {
   const mY = evt.pageY;
   const distance = calculateDistance(switchEl, mX, mY);
 
-  const distanceMinToTriggerAction = 50;
-  if (distance > distanceMinToTriggerAction) isMouseFarEnough = true;
-  if (distance < distanceMinToTriggerAction && !isMouseFarEnough) return;
+  if (distance > config.distanceMinToTriggerAction) isMouseFarEnough = true;
+  if (distance < config.distanceMinToTriggerAction && !isMouseFarEnough) return;
 
   randomMousemoveAction();
 
@@ -42,14 +65,22 @@ function manageMousemoveAction(evt: MouseEvent) {
 }
 
 function setSwitchOff() {
-  switchControl.checked = false;
-  switchControl.disabled = false;
+  const collisionEl = document.querySelector('.collision');
+  if (!collisionEl) return;
+
+  collisionEl.classList.remove('collision--hidden');
+
+  setTimeout(() => {
+    collisionEl.classList.add('collision--hidden');
+    switchControl.checked = false;
+    switchControl.disabled = false;
+  }, 250);
 }
 
 function randomClickAction(): void {
   setTimeout(() => {
     setSwitchOff();
-  }, 2000);
+  }, 1000);
   console.log('randomClickAction');
 }
 
@@ -58,18 +89,19 @@ function manageSwitchEvent(evt: Event): void {
   switchControl.disabled = true;
 
   actionCounter++;
-  if (actionCounter < 3) {
+  if (actionCounter < config.initialBasicMoves) {
     randomClickAction();
     return;
   }
 
-  const randomProbability = getRandomProbabilities(100);
-  const doesNextMoveHasOnMoveAction = randomProbability < 20;
+  const randomProbability = getRandomProbabilities(config.chances.mousemove.total);
+  const doesNextMoveHasOnMoveAction = randomProbability < config.chances.mousemove.active;
   if (!doesNextMoveHasOnMoveAction) {
     randomClickAction();
     return;
   }
 
+  manageFirstMousemove();
   document.addEventListener('mousemove', manageMousemoveAction);
 }
 
