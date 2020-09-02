07f0263e5a2603e8f12511c31ff34331c2fd3e8d
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-09-02 22:50:11 +0200

    Add stats in console

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ export function getRandomProbabilities(multiplier: number = 100): number {
   return Math.floor(Math.random() * multiplier);
 }
 
+export function isDevMode(): boolean {
+  return window.location.href.includes('localhost');
+}
+
 export function isGifExt(file: string): boolean {
   return /.*\.gif/.test(file);
 }
